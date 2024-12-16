e0700561d2758dfdf01892c01105b19510af2b4a
Adil <hello_adil@outlook.com>
2024-12-16 17:56:02 +0500

    added update pass

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const nodemailer = require("nodemailer");
 
 exports.signup = async (req, res) => {
 	try {
@@ -19,7 +20,7 @@ exports.signup = async (req, res) => {
 	}
 };
 
-exports.signin = async(req, res) => {
+exports.signin = async (req, res) => {
 	try {
 		const {email, password} = req.body;
 
@@ -35,7 +36,60 @@ exports.signin = async(req, res) => {
 		} else {
 			res.status(400).send({msg: "Password is incorrect!"});
 		}
+	} catch (error) {
+		console.log(error);
+	}
+};
 
+exports.trigger_update_password = async (req, res) => {
+	try {
+		const {email} = req.body;
+
+		const user = await User.find({email: email});
+
+		if (user.length == 0) {
+			res.status(400).send({msg: "User does not exists!"});
+		}
+
+		const transporter = nodemailer.createTransport({
+			host: "live.smtp.mailtrap.io",
+			port: 587,
+			auth: {
+				user: "api",
+				pass: "ecd375648f87dfb3bf64c2adfb59aca5",
+			},
+		});
+		
+		const mailOptions = {
+			from: "your-email@demomailtrap.com",
+			to: email,
+			subject: "Password Reset",
+			text: `Click the following link to reset your password: http://localhost:3000/reset-password?user=${user[0]._id}`,
+		};
+
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				console.log(error);
+				res.status(500).send("Error sending email");
+			} else {
+				console.log(`Email sent: ${info.response}`);
+				res.status(200).send("Check your email for instructions on resetting your password");
+			}
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+exports.update_password = async (req, res) => {
+	try {
+		const {user_ID, password} = req.body;
+
+		console.log(user_ID, password);
+
+		const user = await User.findOneAndUpdate({_id: user_ID}, {password: password});
+
+		res.status(200).send({msg: "Password Updated!"});
 	} catch (error) {
 		console.log(error);
 	}
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-const {signup, signin} = require("../controllers/auth");
+const {signup, signin, update_password, trigger_update_password} = require("../controllers/auth");
 
 router.post("/signup", signup);
 
-router.post("/signin", signin);
+router.post("/signin", signin); 
+
+router.post("/trigger_update_password", trigger_update_password); 
+
+router.post("/update_password", update_password); 
 
 module.exports = router;
